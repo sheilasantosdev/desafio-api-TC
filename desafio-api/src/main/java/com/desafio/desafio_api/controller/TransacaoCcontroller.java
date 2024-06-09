55ebcea00e8f5030be87a4c8861508b4a4792dfc
Sheila <sheiladeveloper@gmail.com>
2024-06-09 20:07:03 -0300

    Criando EndPoint DeleteAll Ok

--- desafio-api/src/main/java/com/desafio/desafio_api/controller/TransacaoCcontroller.java
+++ desafio-api/src/main/java/com/desafio/desafio_api/controller/TransacaoCcontroller.java
@@ -4,10 +4,7 @@ import com.desafio.desafio_api.model.Transacao;
 import com.desafio.desafio_api.service.TransacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transacao")
@@ -27,4 +24,9 @@ public class TransacaoCcontroller {
         return ResponseEntity.status(201).build(); //(HttpStatus.CREATED).build
     }
 
+    @DeleteMapping
+    public ResponseEntity deleteAll() {
+        transacaoService.deleteAll();
+        return ResponseEntity.status(200).build();
+    }
 }
--- desafio-api/src/main/java/com/desafio/desafio_api/service/TransacaoService.java
+++ desafio-api/src/main/java/com/desafio/desafio_api/service/TransacaoService.java
@@ -27,4 +27,8 @@ public class TransacaoService {
             throw new RuntimeException();
         }
     }
+
+    public void deleteAll() {
+        transacaoList.clear();
+    }
 }
